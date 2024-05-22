a5402fa306878bab14e94b2603e26a02cf09ad17
karolch <karolwtf1@gmail.com>
2024-05-22 22:48:19 +0200

    feat: Collision detection, changing mountains to buildings, adding buildings as ground layer

--- src/createMountain.js
+++ src/createMountain.js
@@ -4,7 +4,7 @@ export default function createMountain(mountainModel, scene) {
     const mountainClone = mountainModel.clone(); // Klonujemy załadowany model
     mountainClone.position.x = Math.random() * 20 - 10;
     mountainClone.position.z = -100;
-    mountainClone.position.y = -6;
+    mountainClone.position.y = Math.random() * 25 - 50;
     mountainClone.rotation.y = 3.14 /2
     scene.add(mountainClone);
     return mountainClone;
